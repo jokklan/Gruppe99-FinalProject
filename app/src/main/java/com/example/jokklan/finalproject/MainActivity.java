edf8cb5faacaface89abab03f7250bdc333fd0a8
jokklan <johanfrolich@gmail.com>
2017-03-29 11:55:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/jokklan/finalproject/MainActivity.java
@@ -0,0 +1,144 @@
+package com.example.jokklan.finalproject;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
+import com.google.android.gms.location.places.Places;
+
+public class MainActivity extends AppCompatActivity implements OnConnectionFailedListener {
+    private GoogleApiClient mGoogleApiClient;
+    public final static int PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, this)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if( mGoogleApiClient != null )
+            mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+                // PERMISSION_REQUEST_ACCESS_FINE_LOCATION can be any unique int
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_ACCESS_FINE_LOCATION);
+            }
+        }
+    }
+
+
+    @Override
+    protected void onStop() {
+        if( mGoogleApiClient != null && mGoogleApiClient.isConnected() ) {
+            mGoogleApiClient.disconnect();
+        }
+        super.onStop();
+    }
+
+    public void findLocation(View view) {
+        Log.d("loc", "Clicked button");
+        guessCurrentPlace();
+    }
+
+    private void guessCurrentPlace() {
+        Log.d("loc", "Starting guessCurrentPlace");
+        PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi.getCurrentPlace( mGoogleApiClient, null );
+
+        Log.d("loc", "Current places are running");
+        result.setResultCallback( new ResultCallback<PlaceLikelihoodBuffer>() {
+            @Override
+            public void onResult( PlaceLikelihoodBuffer likelyPlaces ) {
+                Log.d("loc", "Runnig location callback");
+
+                int count = likelyPlaces.getCount();
+
+                Log.d("loc", "Likely places status: " + likelyPlaces.getStatus());
+                Log.d("loc", "Likely places count: " + count);
+
+                if(count > 0) {
+                    TextView locationTextView = (TextView) findViewById(R.id.locationMessage);
+                    PlaceLikelihood placeLikelihood = likelyPlaces.get(0);
+                    String content = "";
+                    if (placeLikelihood != null && placeLikelihood.getPlace() != null && !TextUtils.isEmpty(placeLikelihood.getPlace().getName()))
+                        content = "Most likely place: " + placeLikelihood.getPlace().getName() + "\n";
+                    if (placeLikelihood != null)
+                        content += "Percent change of being there: " + (int) (placeLikelihood.getLikelihood() * 100) + "%";
+                    locationTextView.setText(content);
+                }
+
+                likelyPlaces.release();
+            }
+        });
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d("loc", "FAILED TO CONNECT (messag): " + connectionResult.getErrorMessage());
+        Log.d("loc", "FAILED TO CONNECT (string): " + connectionResult.toString());
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+}
